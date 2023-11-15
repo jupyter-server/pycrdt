c48050443ba7e5118c17bd6ba6fd4064bedd88d5
David Brochart <david.brochart@gmail.com>
2023-11-15 11:01:46 +0100

    Fix event export

--- python/pycrdt/__init__.py
+++ python/pycrdt/__init__.py
@@ -1,14 +1,8 @@
-from ._pycrdt import (
-    ArrayEvent as ArrayEvent,
-)
-from ._pycrdt import (
-    MapEvent as MapEvent,
-)
-from ._pycrdt import (
-    TextEvent as TextEvent,
-)
 from ._pycrdt import TransactionEvent as TransactionEvent
 from .array import Array as Array
+from .array import ArrayEvent as ArrayEvent
 from .doc import Doc as Doc
 from .map import Map as Map
+from .map import MapEvent as MapEvent
 from .text import Text as Text
+from .text import TextEvent as TextEvent
