ed9a1d7d65ca50b139b032cf5a1acb3d4cfb9b5c
David Brochart <david.brochart@gmail.com>
2024-07-24 11:45:22 +0200

    Support transaction origin

--- python/pycrdt/_transaction.py
+++ python/pycrdt/_transaction.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from types import TracebackType
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 from ._pycrdt import Transaction as _Transaction
 
@@ -14,15 +14,26 @@ class Transaction:
     _txn: _Transaction | None
     _nb: int
 
-    def __init__(self, doc: Doc, _txn: _Transaction | None = None) -> None:
+    def __init__(self, doc: Doc, _txn: _Transaction | None = None, *, origin: Any = None) -> None:
         self._doc = doc
         self._txn = _txn
+        self._origin = origin
         self._nb = 0
+        if origin is None:
+            self._origin = None
+        else:
+            try:
+                self._origin = hash(origin)
+            except Exception:
+                raise TypeError("Transaction origin must be hashable")
 
     def __enter__(self) -> Transaction:
         self._nb += 1
         if self._txn is None:
-            self._txn = self._doc._doc.create_transaction()
+            if self._origin is not None:
+                self._txn = self._doc._doc.create_transaction_with_origin(self._origin)
+            else:
+                self._txn = self._doc._doc.create_transaction()
         self._doc._txn = self
         return self
 
@@ -42,6 +53,13 @@ class Transaction:
             self._txn.drop()
             self._txn = None
             self._doc._txn = None
+
+    @property
+    def origin(self) -> int:
+        if self._txn is None:
+            raise RuntimeError("No current transaction")
+
+        return self._txn.origin()
 
 
 class ReadTransaction(Transaction):
