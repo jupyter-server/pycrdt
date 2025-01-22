287579bd310ff1cdc82c87839084dc9455da4c55
David Brochart <david.brochart@gmail.com>
2025-01-22 10:50:09 +0100

    Fix typing

--- python/pycrdt/_version.py
+++ python/pycrdt/_version.py
@@ -1,6 +1,6 @@
 try:
     from importlib.metadata import version
 except ImportError:
-    from importlib_metadata import version
+    from importlib_metadata import version  # type: ignore[import-not-found, no-redef]
 
 __version__ = version("pycrdt")
