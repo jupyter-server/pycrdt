61ba8c9d47879dd07accf27b2d98d7f489124af9
David Brochart <david.brochart@gmail.com>
2025-01-22 11:05:38 +0100

    Fix typing

--- python/pycrdt/_version.py
+++ python/pycrdt/_version.py
@@ -1,7 +1,7 @@
 try:
     from importlib.metadata import PackageNotFoundError, version
 except ImportError:  # pragma: no cover
-    from importlib_metadata import (  # type: ignore[import-not-found, no-redef]
+    from importlib_metadata import (  # type: ignore[assignment, import-not-found, no-redef]
         PackageNotFoundError,
         version,
     )
