28a529f8d9e59ddadee2ca016a49327b80709045
David Brochart <david.brochart@gmail.com>
2025-04-11 15:28:41 +0200

    -

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,12 +3,6 @@ import gc
 import pytest
 
 
-@pytest.fixture(scope="session", autouse=True)
-def disable_gc(request):
-    gc.disable()
-    request.addfinalizer(gc.enable)
-
-
 @pytest.fixture(scope="function", autouse=True)
 def collect_gc(request):
     request.addfinalizer(gc.collect)
--- tests/test_threads.py
+++ tests/test_threads.py
@@ -10,22 +10,17 @@ pytestmark = pytest.mark.anyio
 async def test_multi_threading():
     doc = Doc()
     doc["text"] = text = Text()
-
-    def callback(event):
-        pass
-
-    subscription = text.observe(callback)
-
-    message = "Hello from thread!"
+    gc.collect()  # drop (write) transaction in this thread
 
     def add_text(text, message):
         text += message
+        gc.collect()  # drop (write) transaction in the other thread
 
+    message = "Hello from thread!"
     limiter = CapacityLimiter(1)
     await to_thread.run_sync(add_text, text, message, limiter=limiter)
     assert str(text) == message
-
-    text.unobserve(subscription)
+    gc.collect()  # drop (read) transaction in this thread
 
     def drop():
         nonlocal text, doc
